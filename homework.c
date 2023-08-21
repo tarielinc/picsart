1aa6dc2b62eedfb8b7e6bb0f20ffed831a4c5612
Tariel <tariel.hambardzumyan@gmail.com>
2023-08-21 21:14:24 +0400

    test 2

--- homework.c
+++ homework.c
@@ -2,7 +2,17 @@
 
 int main(){
     
-    
+    char str[] = "Hello world";
 
+    char* ptr = &str;
+    int a = 0;
+
+    printf("%d\n", a);
+
+    for (int i = 0; str[i]!=\0; i++)
+    {
+        
+    }
+    
 
 }
