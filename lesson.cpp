27922e64f687ed6cac5010ab0951d58d83048d99
Tariel <tariel.hambardzumyan@gmail.com>
2023-09-02 12:05:37 +0400

    C++ Lesson 4

--- lesson.cpp
+++ lesson.cpp
@@ -1,15 +1,7 @@
 #include <iostream>
-#define cmp(a,b) (a)>(b)?(a):b
-
-
 
 int main(){
 
-    int c = 2;
-    int d = 4;
-
-    cmp(++c,d);
-    std::cout << "Welcome to wave " << std::ends;
-    return 0;
+    
 
 }
